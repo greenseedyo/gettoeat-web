c4fdd54fd6c8cd56eaeef09dbbb9b3882a4b3c27
yo <turtleegg@gmail.com>
2018-06-24 15:13:43 +0800

    fix performance issue

--- helpers/StatHelper.php
+++ helpers/StatHelper.php
@@ -44,14 +44,6 @@ class StatHelper
             throw new StatHelperException('interval not set');
         }
 
-        $start_at = $this->start_datetime->getTimestamp();
-        $end_at = $this->end_datetime->getTimestamp();
-        $bills = $this->store->bills->search("`ordered_at` BETWEEN {$start_at} AND {$end_at}")->order('ordered_at ASC');
-        $bill_datasets = $bills->toArray();
-        $bill_ids = array_keys($bill_datasets);
-        $bill_item_datasets = BillItem::search(1)->searchIn('bill_id', $bill_ids)->toArray();
-        $bill_discount_datasets = BillDiscount::search(1)->searchIn('bill_id', $bill_ids)->toArray();
-
         $stat_result = new StatResult();
         $stat_items = array('總營收', '折扣');
         $stat_chart = $stat_result->createChart('總覽', $stat_items);
@@ -64,24 +56,18 @@ class StatHelper
             $period_name = date('Y-m-d(D) H:i', $tmp_start_at);
             $price_sum = 0;
             $discount_sum = 0;
-            foreach ($bill_datasets as $i => $bill_data) {
-                if ($bill_data['ordered_at'] > $tmp_end_at) {
-                    break;
-                }
-                foreach ($bill_item_datasets as $j => $item_data) {
-                    if ($item_data['bill_id'] == $bill_data['id']) {
-                        $price_sum += $item_data['unit_price'] * $item_data['amount'];
-                        unset($bill_item_datasets[$j]);
-                    }
-                }
-                foreach ($bill_discount_datasets as $j => $discount_data) {
-                    if ($discount_data['bill_id'] == $bill_data['id']) {
-                        $discount_sum += $discount_data['value'];
-                        unset($bill_discount_datasets[$j]);
-                    }
-                }
-                unset($bill_datasets[$i]);
+
+            $bill_ids = $this->store->bills->search("`ordered_at` BETWEEN {$tmp_start_at} AND {$tmp_end_at}")->toArray('id');
+            $bill_items = BillItem::search(1)->searchIn('bill_id', $bill_ids);
+            $bill_discounts = BillDiscount::search(1)->searchIn('bill_id', $bill_ids);
+
+            foreach ($bill_items as $bill_item) {
+                $price_sum += $bill_item->getTotalPrice();
             }
+            foreach ($bill_discounts as $bill_discount) {
+                $discount_sum += $bill_discount->value;
+            }
+
             $dataset = array(
                 '總營收' => $price_sum,
                 '折扣' => $discount_sum
@@ -100,14 +86,6 @@ class StatHelper
             throw new StatHelperException('interval not set');
         }
 
-        $start_at = $this->start_datetime->getTimestamp();
-        $end_at = $this->end_datetime->getTimestamp();
-        $bills = $this->store->bills->search("`ordered_at` BETWEEN {$start_at} AND {$end_at}")->order('ordered_at ASC');
-        $bill_datasets = $bills->toArray();
-        $bill_ids = array_keys($bill_datasets);
-        $bill_item_datasets = BillItem::search(1)->searchIn('bill_id', $bill_ids)->toArray();
-        $product_category_ids = $this->store->products->toArray('category_id');
-
         $categories = $this->store->categories;
         $category_names = $categories->toArray('name');
 
@@ -124,20 +102,16 @@ class StatHelper
             $period_name = date('Y-m-d(D) H:i', $tmp_start_at);
             $turnover_dataset = array_fill_keys($stat_items, 0);
             $quantity_dataset = array_fill_keys($stat_items, 0);
-            foreach ($bill_datasets as $i => $bill_data) {
-                if ($bill_data['ordered_at'] > $tmp_end_at) {
-                    break;
-                }
-                foreach ($bill_item_datasets as $j => $item_data) {
-                    if ($item_data['bill_id'] == $bill_data['id']) {
-                        $category_id = $product_category_ids[$item_data['product_id']];
-                        $category_name = $category_names[$category_id];
-                        $turnover_dataset[$category_name] += $item_data['unit_price'] * $item_data['amount'];
-                        $quantity_dataset[$category_name] += $item_data['amount'];
-                        unset($bill_item_datasets[$j]);
-                    }
-                }
-                unset($bill_datasets[$i]);
+
+            $bill_ids = $this->store->bills->search("`ordered_at` BETWEEN {$tmp_start_at} AND {$tmp_end_at}")->toArray('id');
+            $bill_items = BillItem::search(1)->searchIn('bill_id', $bill_ids);
+            $product_category_ids = $this->store->products->toArray('category_id');
+
+            foreach ($bill_items as $key => $bill_item) {
+                $category_id = $product_category_ids[$bill_item->product_id];
+                $category_name = $category_names[$category_id];
+                $turnover_dataset[$category_name] += $bill_item->getTotalPrice();
+                $quantity_dataset[$category_name] += $bill_item->amount;
             }
             $turnover_chart->append($period_name, $turnover_dataset);
             $quantity_chart->append($period_name, $quantity_dataset);
