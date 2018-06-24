f7e9667d39064dac934666ef8b62c8da6443b47a
yo <turtleegg@gmail.com>
2018-06-24 16:32:31 +0800

    add customers stat

--- helpers/StatHelper.php
+++ helpers/StatHelper.php
@@ -32,6 +32,8 @@ class StatHelper
         switch ($this->topic) {
         case 'category':
             return $this->getCategoryStatResult();
+        case 'customers':
+            return $this->getCustomersStatResult();
         case 'overview':
         default:
             return $this->getOverviewStatResult();
@@ -73,6 +75,42 @@ class StatHelper
                 '折扣' => $discount_sum
             );
             $stat_chart->append($period_name, $dataset);
+
+            $tmp_start_datetime = $tmp_end_datetime;
+        }
+
+        return $stat_result;
+    }
+
+    private function getCustomersStatResult(): StatResult
+    {
+        if (!isset($this->interval)) {
+            throw new StatHelperException('interval not set');
+        }
+
+        $stat_result = new StatResult();
+        $count_stat_chart = $stat_result->createChart('來客數統計', array('總來客數'));
+        $avg_stat_chart = $stat_result->createChart('平均客單價', array('平均客單價'));
+
+        while (!isset($tmp_end_datetime) or $tmp_end_datetime < $this->end_datetime) {
+            $tmp_start_datetime = $tmp_start_datetime ?: $this->start_datetime;
+            $tmp_start_at = $tmp_start_datetime->getTimestamp();
+            $tmp_end_datetime = (new Datetime())->setTimestamp($tmp_start_at)->add($this->interval);
+            $tmp_end_at = $tmp_end_datetime->getTimestamp();
+            $period_name = date('Y-m-d(D) H:i', $tmp_start_at);
+
+            $bills = $this->store->bills->search("`ordered_at` BETWEEN {$tmp_start_at} AND {$tmp_end_at}");
+            $customers_sum = $bills->sum('custermers');
+            $price_sum = $bills->sum('price');
+            $avg_price = 0;
+            if ($customers_sum > 0) {
+                $avg_price = round($price_sum / $customers_sum, 2);
+            }
+
+            $dataset = array('總來客數' => $customers_sum);
+            $count_stat_chart->append($period_name, $dataset);
+            $dataset = array('平均客單價' => $avg_price);
+            $avg_stat_chart->append($period_name, $dataset);
 
             $tmp_start_datetime = $tmp_end_datetime;
         }
