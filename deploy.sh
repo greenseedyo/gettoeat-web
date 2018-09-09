65f786c78386dcd1afb9d07fedf0b812022e97bb
yo <turtleegg@gmail.com>
2018-09-10 01:56:15 +0800

    update rsync path

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 date > fingerprint.txt
-rsync -av --delete /Users/yo/docker/gettoeat-docker/code/gettoeat-web/ yo@13.112.82.166:/gettoeat/gettoeat-docker/code/gettoeat-web/
+rsync -av --delete /Users/yo/docker/gettoeat-docker/code/gettoeat-web/ yo@13.112.82.166:/usr/share/nginx/html/code/gettoeat-web/
