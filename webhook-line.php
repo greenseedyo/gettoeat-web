3caa870232dfb211342ecd2f8c7cb076cf0fbb35
yo <turtleegg@gmail.com>
2018-11-04 02:56:18 +0800

    add reply message case

--- webhook-line.php
+++ webhook-line.php
@@ -5,7 +5,8 @@ require_once(ROOT_DIR . '/helpers/StatHelper.php');
 if ('development' == $environment) {
     //$msg = '{"events":[{"type":"join","replyToken":"6cc68fd121f54d3ca479fb63c55f4f32","source":{"groupId":"Cfd0e257d93a9346c5ba7a48d2f4caae1","type":"group"},"timestamp":1541224257292}]}';
     //$msg = '{"events":[{"type":"message","replyToken":"2c0ed637105845f78964419efe51a090","source":{"groupId":"Cfd0e257d93a9346c5ba7a48d2f4caae1","userId":"U8d06f9b05c23c2e1279dce883a3d3dc5","type":"group"},"timestamp":1541263944809,"message":{"type":"text","id":"8811946762260","text":"本月營收"}}]}';
-    $msg = '{"events":[{"type":"message","replyToken":"c1633358289645e79aa0b9bd34195513","source":{"groupId":"Cfd0e257d93a9346c5ba7a48d2f4caae1","userId":"U8d06f9b05c23c2e1279dce883a3d3dc5","type":"group"},"timestamp":1541188811207,"message":{"type":"text","id":"8807694197168","text":"今誰收"}}]}';
+    //$msg = '{"events":[{"type":"message","replyToken":"c1633358289645e79aa0b9bd34195513","source":{"groupId":"Cfd0e257d93a9346c5ba7a48d2f4caae1","userId":"U8d06f9b05c23c2e1279dce883a3d3dc5","type":"group"},"timestamp":1541188811207,"message":{"type":"text","id":"8807694197168","text":"今誰收"}}]}';
+    $msg = '{"events":[{"type":"message","replyToken":"c1633358289645e79aa0b9bd34195513","source":{"groupId":"Cfd0e257d93a9346c5ba7a48d2f4caae1","userId":"U8d06f9b05c23c2e1279dce883a3d3dc5","type":"group"},"timestamp":1541188811207,"message":{"type":"text","id":"8807694197168","text":"各位 營收匯一下"}}]}';
 } else {
     $msg = file_get_contents('php://input');
 }
@@ -31,6 +32,34 @@ function getTotalSales($store, $start_date, $end_date)
     $datasets = $chart->getDataSets();
     $total_sales = $datasets['總營收'][0];
     return $total_sales;
+}
+
+function getStaffAdjustmentsLastMonth($store)
+{
+    $start_date = date('Y-m-01', strtotime('last month'));
+    $end_date = date('Y-m-d', strtotime('last day of last month'));
+    $day_change_interval = new DateInterval("PT{$store->getDateChangeAt()}H");
+    $start_datetime = (new Datetime($start_date))->add($day_change_interval);
+    $end_datetime = (new Datetime($end_date))->add(new DateInterval('P1D'))->add($day_change_interval);
+    $period_interval = $start_datetime->diff($end_datetime);
+    $helper = new StatHelper($store);
+    $helper->setTopic('shift');
+    $helper->setInterval($period_interval);
+    $helper->setStartDatetime($start_datetime);
+    $helper->setEndDatetime($end_datetime);
+    $stat_result = $helper->getStatResult();
+    $chart = $stat_result->getChart('收款人結餘金額');
+    $names = $chart->getXAxisCategories();
+    $datasets = $chart->getDataSets();
+    $amount = $datasets['金額'];
+    $data = array();
+    if (empty($names)) {
+        return array();
+    }
+    foreach ($names as $key => $name) {
+        $data[$name] = $amount[$key];
+    }
+    return $data;
 }
 
 $data = json_decode($msg);
@@ -78,6 +107,14 @@ case 'message':
         $end_date = date('Y-m-d', strtotime('last day of last month'));
         $total_sales = getTotalSales($store, $start_date, $end_date);
         $reply_message = sprintf("%s月營收共 $%s", date('m', strtotime('last month')), $total_sales);
+    } elseif (strstr($text, '各位') and strstr($text, '收') and strstr($text, '匯')) {
+        $result = getStaffAdjustmentsLastMonth($store);
+        $lines = array();
+        $lines[] = sprintf("[%s月收帳統計]", date('m', strtotime('last month')));
+        foreach ($result as $name => $amount) {
+            $lines[] = sprintf("%s: %s%s", $name, $currency_symbol, $amount);
+        }
+        $reply_message = implode(PHP_EOL, $lines);
     } elseif (strstr($text, '今') and strstr($text, '誰') and strstr($text, '收')) {
         $staffs = $store->staffs->toArray('name');
         $key = array_rand($staffs);
@@ -90,7 +127,11 @@ case 'leave':
 }
 
 if ($reply_message) {
-    $line_bot_chat->replyMessage($reply_token, $reply_message);
+    if ('development' == $environment) {
+        $line_bot_chat->pushMessage($reply_message);
+    } else {
+        $line_bot_chat->replyMessage($reply_token, $reply_message);
+    }
 }
 
 if ($push_message) {
