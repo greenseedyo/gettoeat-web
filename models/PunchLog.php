ec37118c1535079f930cb3a29e896bad5cbdadd4
yo <turtleegg@gmail.com>
2019-01-04 04:14:43 +0800

    punch db migration

---
+++ models/PunchLog.php
@@ -0,0 +1,44 @@
+<?php
+
+class PunchLogRow extends Pix_Table_Row
+{
+    public function preInsert()
+    {
+        $this->created_at = $this->created_at ?: time();
+        $this->updated_at = $this->updated_at ?: time();
+    }
+
+    public function preUpdate($changed_fields = null)
+    {
+        $this->updated_at = $this->updated_at ?: time();
+    }
+}
+
+class PunchLog extends Pix_Table
+{
+    public $_rowClass = 'PunchLogRow';
+
+    public function init()
+    {
+        $this->_name = 'punch_log';
+        $this->_primary = array('id');
+
+        $this->_columns['id'] = array('type' => 'int', 'auto_increment' => true, 'unsigned' => true);
+        $this->_columns['staff_id'] = array('type' => 'int', 'unsigned' => true);
+        $this->_columns['type'] = array('type' => 'tinyint', 'unsigned' => true);
+        $this->_columns['timestamp'] = array('type' => 'int', 'unsigned' => true);
+        $this->_columns['created_at'] = array('type' => 'int', 'unsigned' => true);
+        $this->_columns['created_by'] = array('type' => 'int', 'unsigned' => true);
+        $this->_columns['created_from'] = array('type' => 'int', 'unsigned' => true);
+        $this->_columns['updated_at'] = array('type' => 'int', 'unsigned' => true);
+        $this->_columns['updated_by'] = array('type' => 'int', 'unsigned' => true);
+        $this->_columns['updated_from'] = array('type' => 'int', 'unsigned' => true);
+
+        $this->addIndex('staff_id', array('staff_id'));
+        $this->addIndex('timestamp', array('timestamp'));
+    }
+
+    const TYPE_IN = 1;
+    const TYPE_OUT = 1;
+}
+
--- models/Staff.php
+++ models/Staff.php
@@ -33,6 +33,7 @@ class Staff extends Pix_Table
         $this->_columns['name'] = array('type' => 'varchar', 'size' => 255);
         $this->_columns['email'] = array('type' => 'varchar', 'size' => 255);
         $this->_columns['phone'] = array('type' => 'varchar', 'size' => 255);
+        $this->_columns['code'] = array('type' => 'varchar', 'size' => 10);
         $this->_columns['off'] = array('type' => 'tinyint', 'unsigned' => true, 'size' => 1);
         $this->_columns['created_at'] = array('type' => 'int', 'unsigned' => true);
         $this->_columns['created_by'] = array('type' => 'int', 'unsigned' => true);
@@ -40,6 +41,7 @@ class Staff extends Pix_Table
         $this->_columns['updated_by'] = array('type' => 'int', 'unsigned' => true);
 
         $this->addIndex('store_id_off', array('store_id', 'off'));
+        $this->addIndex('store_id_code', array('store_id', 'code'));
 
         $this->_relations['group'] = array('rel' => 'has_one', 'type' => 'StaffGroup', 'foreign_key' => 'group_id');
     }
